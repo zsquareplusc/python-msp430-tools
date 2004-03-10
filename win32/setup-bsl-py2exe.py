a97f2da3744a166ad7dde86edb058c98e6ccc57a
cliechti <>
2004-03-10 12:15:25 +0000

    bugfix

--- win32/setup-bsl-py2exe.py
+++ win32/setup-bsl-py2exe.py
@@ -1,10 +1,11 @@
 # setup script for py2exe to create the msp430-bsl.exe
-# $Id: setup-bsl-py2exe.py,v 1.1 2004/03/01 00:11:55 cliechti Exp $
+# $Id: setup-bsl-py2exe.py,v 1.2 2004/03/10 13:15:25 cliechti Exp $
 
 from distutils.core import setup
 import glob, sys, py2exe, os
 
 os.chdir('..')
+sys.path.append('.')
 
 sys.argv.append("py2exe")
 

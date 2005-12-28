bd052e7f021eb1703a708a06aae15b9e68bf95b2
cliechti <>
2005-12-28 21:02:27 +0000

    add real command line options (-h, -o)

--- ihex2titext.py
+++ ihex2titext.py
@@ -1,25 +1,46 @@
 #!/usr/bin/env python
-# simple intel hex to TI text converter
+# simple intel hex (and elf) to TI text converter
 # data is read from stdin and output on stdout
 # usage: cat file.a43 | ihex2titext >out.txt
+# usage: ihex2titext file.a43 >out.txt
+# usage: ihex2titext file.a43 -o out.txt
 #
 # (C) 2005 Chris Liechti <cliechti@gmx.net>
 # this is distributed under a free software license, see license.txt
 #
-# $Id: ihex2titext.py,v 1.2 2005/12/28 20:37:57 cliechti Exp $
+# $Id: ihex2titext.py,v 1.3 2005/12/28 22:02:27 cliechti Exp $
 
-#~ from msp430.util import hexdump
 from msp430 import memory
 import sys
 
-data = memory.Memory()
-if len(sys.argv) > 1:
-    data.loadFile(sys.argv[1])
+from optparse import OptionParser
+
+parser = OptionParser(usage='usage: %prog [-o filename.txt|-] file.a43|-')
+parser.add_option("-o", "--output", dest="output",
+                  help="write result to given file", metavar="FILE")
+(options, args) = parser.parse_args()
+
+if not args:
+    parser.error('no input files given')
+
+#prepare output
+if options.output is None or options.output == '-':
+    out = sys.stdout
 else:
-    data.loadIHex(sys.stdin)                    #intel-hex
+    out = file(options.output, 'wb')
+
+#get input
+data = memory.Memory()
+
+for filename in args:
+    if filename == '-':
+        data.loadIHex(sys.stdin)                #intel-hex
+    else:
+        data.loadFile(filename)
+
+#output TI-Text
 for segment in data:
-    #~ hexdump((segment.startaddress, segment.data))            #print a hex display
-    sys.stdout.write("@%04x\n" % segment.startaddress)
+    out.write("@%04x\n" % segment.startaddress)
     for i in range(0, len(segment.data), 16):
-        sys.stdout.write("%s\n" % " ".join(["%02x" % ord(x) for x in segment.data[i:i+16]]))
-sys.stdout.write("q\n")
+        out.write("%s\n" % " ".join(["%02x" % ord(x) for x in segment.data[i:i+16]]))
+out.write("q\n")
--- titext2ihex.py
+++ titext2ihex.py
@@ -1,23 +1,45 @@
 #!/usr/bin/env python
 # simple TI text to intel hex converter
 # data is read from stdin and output on stdout
-# usage: cat file.txt | titext2ihex >out.a43
+# usage: cat file.txt | titext2ihex - >out.a43
+# usage: titext2ihex file.txt >out.a43
+# usage: titext2ihex file.txt -o out.a43
 #
 # (C) 2004 Chris Liechti <cliechti@gmx.net>
 # this is distributed under a free software license, see license.txt
 #
-# $Id: titext2ihex.py,v 1.2 2005/12/28 20:37:57 cliechti Exp $
+# $Id: titext2ihex.py,v 1.3 2005/12/28 22:02:27 cliechti Exp $
 
-from msp430.util import hexdump, makeihex, _ihexline
+from msp430.util import makeihex, _ihexline
 from msp430 import memory
 import sys
 
-data = memory.Memory()                      #prepare downloaded data
-if len(sys.argv) > 1:
-    data.loadFile(sys.argv[1])
+from optparse import OptionParser
+
+parser = OptionParser(usage='usage: %prog [-o filename.a43|-] file.txt|-')
+parser.add_option("-o", "--output", dest="output",
+                  help="write result to given file", metavar="FILE")
+(options, args) = parser.parse_args()
+
+if not args:
+    parser.error('no input files given')
+
+#prepare output
+if options.output is None or options.output == '-':
+    out = sys.stdout
 else:
-    data.loadTIText(sys.stdin)                  #TI's format
+    out = file(options.output, 'wb')
+
+#get input
+data = memory.Memory()                      #prepare downloaded data
+
+for filename in args:
+    if filename == '-':
+        data.loadTIText(sys.stdin)          #TI's format
+    else:
+        data.loadFile(filename)
+
+#write ihex file
 for segment in data:
-    #~ hexdump((segment.startaddress, segment.data))            #print a hex display
-    makeihex((segment.startaddress, segment.data), eof=0)           #ouput a intel-hex file
-_ihexline(0, [], type=1)
+    makeihex((segment.startaddress, segment.data), eof=0, output=out)
+_ihexline(0, [], type=1, output=out)

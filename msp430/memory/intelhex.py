0ee1343bdaf4a77886a61df0a1089e014f9e46a3
Chris Liechti <cliechti@gmx.net>
2011-01-02 04:16:51 +0100

    add support for writing files with contents above 64k

--- msp430/memory/intelhex.py
+++ msp430/memory/intelhex.py
@@ -3,6 +3,7 @@ Helper functions to read and write intel hex files.
 """
 
 import sys
+import struct
 import msp430.memory
 import msp430.memory.error
 
@@ -34,10 +35,10 @@ def load(filelike):
                 segmentdata.append(chr(int(l[9+2*i:11+2*i],16)))
             currentAddr = length + currentAddr
         elif type == 0x02:
-            extendAddr = int(l[9:13],16) << 4
+            extendAddr = int(l[9:13], 16) << 4
         elif type == 0x04 :
             extendAddr = int(l[9:13], 16) << 16
-        elif type in (0x01, 0x03, 0x04, 0x05):
+        elif type in (0x01, 0x03, 0x05):
             pass
         else:
             sys.stderr.write("Ignored unknown field (type 0x%02x) in ihex file.\n" % type)
@@ -51,9 +52,17 @@ def save(memory, filelike):
     noeof=0
     for seg in sorted(memory.segments):
         address = seg.startaddress
-        data    = seg.data
+        data = seg.data
         start = 0
+        last_upper_address_bits = 0
         while start < len(data):
+            # check for addresses >64k and output offset command when the value
+            # changes
+            upper_address_bits = address >> 16
+            if last_upper_address_bits != upper_address_bits:
+                filelike.write(_ihexline(address, struct.pack(">H", upper_address_bits), record_type=4))  # set offset
+                last_upper_address_bits = upper_address_bits
+            # write data line
             end = start + 16
             if end > len(data): end = len(data)
             filelike.write(_ihexline(address, data[start:end]))
@@ -62,15 +71,13 @@ def save(memory, filelike):
     filelike.write(_ihexline(0, [], end=True))   # append no data but an end line
 
 
-def _ihexline(address, buffer, end=False):
+def _ihexline(address, buffer, end=False, record_type=0):
     """internal use: generate a line with intel hex encoded data"""
     out = []
-    if end:
-        type = 1
-    else:
-        type = 0
-    out.append( ':%02X%04X%02X' % (len(buffer), address & 0xffff, type) )
-    sum = len(buffer) + ((address >> 8) & 255) + (address & 255) + (type & 255)
+    if end: # special override if end parameter is given
+        record_type = 1
+    out.append(':%02X%04X%02X' % (len(buffer), address & 0xffff, record_type))
+    sum = len(buffer) + ((address >> 8) & 255) + (address & 255) + (record_type & 255)
     for b in [ord(x) for x in buffer]:
         out.append('%02X' % (b & 255))
         sum += b&255

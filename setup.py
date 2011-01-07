83cd4be6d23623086239259880766a8d7d677fd7
Chris Liechti <cliechti@gmx.net>
2011-01-07 01:44:28 +0100

    updates to setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,11 @@
-# setup.py
+# This is a setup script for pythons distutils. It will install the
+# python-msp430-tools extension when run as: python setup.py install
+
+# Author: Chris Liechti <cliechti@gmx.net>
+#
+# This is open source software under the BSD license. See LICENSE.txt for more
+# details.
+
 
 from distutils.core import setup
 import sys
@@ -6,18 +13,18 @@ import sys
 setup(
     name="python-msp430-tools",
     description="Python MSP430 tools",
-    version="0.5",
+    version="0.1",
     author="Chris Liechti",
     author_email="cliechti@gmx.net",
-    url="http://python-msp430-tools/",
+    url="http://launchpad.net/python-msp430-tools/",
     packages=['msp430'],
     license="Python",
-    long_description="Python tools for the MSP430 processor including BSL, JTAG",
+    long_description=open('README.txt').read(),
     classifiers = [
         'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Developers',
         'Intended Audience :: End Users/Desktop',
-        'License :: OSI Approved :: Python Software Foundation License',
+        'License :: OSI Approved :: BSD License',
         'Natural Language :: English',
         'Operating System :: POSIX',
         'Operating System :: Microsoft :: Windows',

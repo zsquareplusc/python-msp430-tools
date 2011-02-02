425ee7c89e704ebeb10034336e014cbb9e39ed7c
Chris Liechti <cliechti@gmx.net>
2011-02-02 01:49:11 +0100

    extend so that multiple files can be watched at once

--- msp430/shell/watch.py
+++ msp430/shell/watch.py
@@ -29,14 +29,16 @@ def get_file_stats(filename):
     else:
         return (0, 0)
 
-def watch(filename, callback):
-    """repeatedly check the file and run the callback when it has changed"""
-    last_stat = get_file_stats(filename)
+
+def watch(filenames, callback):
+    """repeatedly check the given files and run the callback when one has changed"""
+
+    last_stats = [get_file_stats(filename) for filename in filenames]
     while True:
-        stats = get_file_stats(filename)
-        if stats != last_stat:
-            last_stat = stats
-            callback(*stats)
+        stats = [get_file_stats(filename) for filename in filenames]
+        if stats != last_stats:
+            last_stats = stats
+            callback()
         time.sleep(2)
 
 
@@ -54,21 +56,16 @@ if __name__ == '__main__':
 
     (options, args) = parser.parse_args()
 
-    if len(args) != 1:
-        parser.error("expected exactly one filename")
-
     if options.execute:
         cmd = options.execute.replace('--', args[0])
-        sys.stderr.write("watch: command lie will be: %r\n" % cmd)
+        sys.stderr.write("watch: command line will be: %r\n" % cmd)
     else:
         cmd = None
-    def execute(size, date):
-        if date:    # only download if file exits
-            sys.stderr.write("watch: file changed %s\n" % datetime.datetime.now())
-            if cmd is not None:
-                #~ sys.stderr.write("watch: execute: %r\n" % cmd)
-                subprocess.call(cmd, shell=True)
-        else:
-            sys.stderr.write("watch: file disappeared\n")
 
-    watch(args[0], callback=execute)
+    def execute():
+        sys.stderr.write("watch: file(s) changed %s\n" % datetime.datetime.now())
+        if cmd is not None:
+            #~ sys.stderr.write("watch: execute: %r\n" % cmd)
+            subprocess.call(cmd, shell=True)
+
+    watch(args, callback=execute)

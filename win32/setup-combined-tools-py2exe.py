561f1dd804d2fa2b30ff9700cf297d6b744fd163
Chris Liechti <cliechti@gmx.net>
2011-09-08 16:01:11 +0200

    add package data to zip (py2exe)

--- win32/setup-combined-tools-py2exe.py
+++ win32/setup-combined-tools-py2exe.py
@@ -4,10 +4,11 @@
 # $Id: setup-combined-tools-py2exe.py,v 1.5 2008/06/05 12:49:59 cliechti Exp $
 
 from distutils.core import setup
-import glob, sys, py2exe, os
+import glob, sys, py2exe, os, zipfile
 
 sys.path.insert(0, os.path.abspath('..'))
 os.chdir('..')
+print "changed directory to %s" % os.path.abspath(os.curdir)
 
 sys.argv.append("py2exe")
 
@@ -17,7 +18,7 @@ setup(
     author_email="cliechti@gmx.net",
     url="http://launchpad.net/python-msp430-tools/",
     
-    version = '0.3',
+    version = '0.5',
     options = { "py2exe":
         {
             'dist_dir': 'bin',
@@ -28,7 +29,7 @@ setup(
             'packages': ['msp430', 'msp430.asm', 'msp430.memory', 'msp430.shell',
                         'msp430.gdb', 'msp430.jtag', 'msp430.bsl', 'msp430.bsl5'],
             'includes': ['pywinusb', 'EasyDialogs'],
-            'dll_excludes': ['HIL.dll', 'MSP430mspgcc.dll'],
+            'dll_excludes': ['HIL.dll', 'MSP430.dll', 'MSP430mspgcc.dll'],
             'optimize': 2,
         }
     },
@@ -45,3 +46,22 @@ setup(
     ],
     zipfile = "lib/shared.zip",
 )
+
+# py2exe does not lool at the package_data option of distutils. add files manually
+package_data = [
+        'msp430/asm/definitions/msp430-mcu-list.txt',
+        'msp430/bsl/BL_150S_14x.txt',
+        'msp430/bsl/BL_150S_44x.txt',
+        'msp430/bsl/BS_150S_14x.txt',
+        'msp430/bsl/patch.txt',
+        'msp430/bsl5/RAM_BSL.00.05.04.34.txt',
+        ]
+
+print "appending package data..."
+archive = zipfile.PyZipFile("bin/lib/shared.zip", 'a')
+for path in package_data:
+    archive.write(
+            path,
+            path,
+            compress_type=zipfile.ZIP_DEFLATED
+            )

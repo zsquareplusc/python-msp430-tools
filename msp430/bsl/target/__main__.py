bf159b7d2bdd8b660d7bfab945d2627b271ba168
Christopher Wilson <cwilson@cdwilson.us>
2012-02-12 20:49:36 -0800

    Add support for custom BSL hardware
    Initial support for the following BSL hardware:
    - Telos B wireless sensor mote
    - Flying Camp Design MSP430 BSL Programmer

---
+++ msp430/bsl/target/__main__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright (c) 2012 Christopher Wilson <cwilson@cdwilson.us>
+# All Rights Reserved.
+# Simplified BSD License (see LICENSE.txt for full text)
+
+import msp430.bsl.target
+msp430.bsl.target.main()

09ade46ae7da7d0e6ddd4b4e14ed0130794d0b5b
Chris Liechti <cliechti@gmx.net>
2018-05-30 13:32:43 +0200

    chore: release 0.9.1

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ import glob
 setup(
     name="python-msp430-tools",
     description="Python MSP430 Tools",
-    version="0.9",
+    version="0.9.1",
     author="Chris Liechti",
     author_email="cliechti@gmx.net",
     url="https://github.com/zsquareplusc/python-msp430-tools",
